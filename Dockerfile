ad6dd438c8550d8b58e1f7a7febd00a40267f31b
CharanDetkrathok <charandetkrathok@gmail.com>
2021-01-04 23:14:42 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+# Use official base image of Java Runtim
+FROM nginx:alpine
+COPY . /usr/share/nginx/html
+
---
+++ test.js
@@ -0,0 +1,9 @@
+function TurnOn() {
+    let turnOnTheLight = document.getElementById('MyImg').src = './pic_bulbon.gif';
+    return turnOnTheLight;
+}
+
+function TurnOff() {
+    let turnOffTheLight = document.getElementById('MyImg').src = './pic_bulboff.gif';
+    return turnOffTheLight;
+}
